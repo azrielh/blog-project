4f5e1c824ecd730e5315c84ee06fced7fd5b7a58
azrielh <azhopkins@gmail.com>
2015-04-05 15:30:59 -0700

    edit and delete posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
 
-  before_action :find_post, only: [:show]
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
 
   def index
     @post = Post.all.order('created_at DESC')
@@ -22,6 +22,24 @@ class PostsController < ApplicationController
   def show
 
   end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      flash[:notice] = "Could not Update!"
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to root_path
+  end
+
 
   private
 
