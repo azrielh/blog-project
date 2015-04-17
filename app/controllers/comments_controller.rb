3685378ad94d60942dc3bc1e742ff9c0dcdc76fe
azrielh <azhopkins@gmail.com>
2015-04-17 10:38:27 -0700

    added ajax comment delete

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -26,7 +26,10 @@ class CommentsController < ApplicationController
     @post = Post.find(params[:post_id])
     @comment = Comment.find(params[:id])
     @comment.destroy
-    redirect_to post_path(@post), notice: "Comment Deleted"
+    respond_to do |format|
+      format.html { redirect_to post_path(@post), notice: "Comment Deleted" }
+      format.js { render }
+    end
   end
 
 end
